475ac8f64ed61b775646541fd45fd4eb3a04c827
Rob van Putten <breinbaasnl@gmail.com>
2022-10-17 10:43:16 +0000

    settings annoyance

--- shared/settings.py
+++ shared/settings.py
@@ -1,20 +1,15 @@
 # locatie van de AHN raster bestanden
-AHN2_DATA = "/home/breinbaas/Documents/ahn2"
-AHN3_DATA = "/home/breinbaas/Documents/ahn3"
-AHN4_DATA = "/home/breinbaas/Documents/ahn4"
+AHN2_DATA = "E:/Klanten/Waternet/ahn2"
+AHN3_DATA = "E:/Klanten/Waternet/ahn3"
+AHN4_DATA = "E:/Klanten/Waternet/ahn4"
 
 AHN2_YEAR = 2009  # see https://www.amsterdam.nl/stelselpedia/ahn-index/inwinnen-ahn/ (actually 2008-2010)
 AHN3_YEAR = 2015  # see https://www.amsterdam.nl/stelselpedia/ahn-index/inwinnen-ahn/
 AHN4_YEAR = 2020  # see https://www.ahn.nl/kwaliteitsbeschrijving
 
-# locatie van verschil bestanden
-AGZ43_DATA = "/home/breinbaas/Documents/agz43"
-AGZ42_DATA = "/home/breinbaas/Documents/agz42"
-AGZ32_DATA = "/home/breinbaas/Documents/agz32"
-
 # locatie van diverse shape bestanden
-SHAPE_FILES = "/home/breinbaas/Documents/shapefiles"
+SHAPE_FILES = "E:/Klanten/Waternet/shapefiles"
 
 # pad naar de dijk info shape
-LEVEESECTIONS_SHAPE = "/home/breinbaas/Documents/shapefiles/dijksegmenten.shp"
-LEVEES_SHAPE = "/home/breinbaas/Documents/shapefiles/dijktrajecten.shp"
+LEVEESECTIONS_SHAPE = "E:/Klanten/Waternet/shapefiles/dijksegmenten.shp"
+LEVEES_SHAPE = "E:/Klanten/Waternet/shapefiles/dijktrajecten.shp"
